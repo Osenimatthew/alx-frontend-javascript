07d6a8b9b059a0920dd0ef262b2489e99b8c8192
Osenimatthew <osenimatthew9@gmail.com>
2024-07-25 09:05:53 -0700

    Done

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,18 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
+      throw new Error('Invalid input type');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Invalid input type');
+    }
